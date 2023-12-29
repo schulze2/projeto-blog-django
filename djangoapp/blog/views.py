75db035f80beb5e0cd7b10be8d1842e1d13f9df0
schulze2 <germano_schulze_junior@hotmail.com>
2023-12-29 09:30:29 -0300

    Tags: usando ManyToManyField dentro do template do Django

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -59,6 +59,25 @@ def category(request, slug):
     )
 
 
+def tag(request, slug):
+    posts = (
+        Post.objManager.get_published()
+        .filter(tags__slug=slug)
+    )
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
 def page(request, slug):
 
     return render(
