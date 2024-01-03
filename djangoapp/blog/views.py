1e55fa1c4c6becd6c18259ef2451a211c0e9f68e
schulze2 <germano_schulze_junior@hotmail.com>
2024-01-03 11:03:11 -0300

    Migrando a view tag para Class Based View (CBV) ListView no views.py do Django

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -5,8 +5,9 @@ from django.shortcuts import render
 from blog.models import Post, Page
 from django.db.models import Q
 from django.contrib.auth.models import User
-from django.http import Http404, HttpRequest, HttpResponse
+from django.http import Http404
 from django.views.generic import ListView
+from .models import Tag
 
 PER_PAGE = 9
 
@@ -118,54 +119,25 @@ class CategoryListView(PostListView):
         return ctx
 
 
-def category(request, slug):
-    posts = (
-        Post.objManager.get_published()
-        .filter(category__slug=slug)
-    )
+class TagListView(PostListView):
+    allow_empty = False
 
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(
+            tags__slug=self.kwargs.get('slug')
+        )
 
-    if len(page_obj) == 0:
-        raise Http404()
-
-    page_title = f'{page_obj[0].category.name} - Categoria - '
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        tag_slug = self.kwargs.get('slug')
+        tag = Tag.objects.get(slug=tag_slug)
+        page_title = (
+            f'Tag - {tag.name} - '
+        )
+        ctx.update({
             'page_title': page_title,
-        }
-    )
-
-
-def tag(request, slug):
-    posts = (
-        Post.objManager.get_published()
-        .filter(tags__slug=slug)
-    )
-
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    if len(page_obj) == 0:
-        raise Http404()
-
-    page_title = f'{page_obj[0].tags.first().name} - Tags - '
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
-            'page_title': page_title,
-        }
-    )
+        })
+        return ctx
 
 
 def search(request):
